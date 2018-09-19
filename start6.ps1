837bfda691b74ed158a152da7fd184b53c4af7d1
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 16:40:04 +0300

    Update and rename start5.ps1 to start6.ps1

--- start6.ps1
+++ start6.ps1
@@ -4,4 +4,4 @@ $password = '1234QWer1234'
 
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
-Start-Process powershell.exe -Credential $credential -ArgumentList ("-file c:/test.ps1")
+Start-Process powershell.exe -Credential $credential -ArgumentList ("-ExecutionPolicy Unrestricted -file c:/test.ps1")
