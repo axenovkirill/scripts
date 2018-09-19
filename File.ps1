c2ba5b3077b256640bbf13c134d1a3a5c904ace3
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 14:32:32 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -2,4 +2,4 @@
 '$list.Add("ru-RU")' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 'Set-WinUserLanguageList $list -Force' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-C:\testq.ps1
+C:\test.ps1
