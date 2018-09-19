6d93f64ee956aa5ed3a7bc479b1e113be8e1ebe6
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 15:20:17 +0300

    Update and rename File4.ps1 to File5.ps1

--- File5.ps1
+++ File5.ps1
@@ -1,6 +1,6 @@
 '$list = Get-WinUserLanguageList' | out-file c:\test.ps1 #создание файла с одной строкой
 '$list.Add("ru-RU")' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 'Set-WinUserLanguageList -LanguageList $list -Force' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Set-ExecutionPolicy RemoteSigned -Force
 c:\test.ps1
 ls
