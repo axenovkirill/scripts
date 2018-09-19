a923664f20377876e90beac66474160e577a8acb
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 12:16:37 +0300

    Update and rename ConfVM.ps1 to ConfDesktop.ps1

--- ConfDesktop.ps1
+++ ConfDesktop.ps1
@@ -1,15 +1,13 @@
-$Install_Path = "C:\"
+$Install_Path = "C:\Users\Public\Desktop"
 $WSShell = New-Object -com WScript.Shell
-$ShortcutPath = Join-Path -Path $Install_Path -ChildPath "List registracii.url"
+$ShortcutPath = Join-Path -Path $Install_Path -ChildPath "List registracii1.url"
 $NewShortcut = $WSShell.CreateShortcut($ShortcutPath)
 $NewShortcut.TargetPath = "https://goo.gl/forms/qfKT57oppr51KgP63"
 $NewShortcut.Save()
-$Install_Path = "C:\"
+$Install_Path = "C:\Users\Public\Desktop"
 $WSShell = New-Object -com WScript.Shell
-$ShortcutPath = Join-Path -Path $Install_Path -ChildPath "Anketa slushatelya.url"
+$ShortcutPath = Join-Path -Path $Install_Path -ChildPath "Anketa slushatelya1.url"
 $NewShortcut = $WSShell.CreateShortcut($ShortcutPath)
 $NewShortcut.TargetPath = "https://docs.google.com/forms/d/e/1FAIpQLSd3u6D2kdBrOuFyZYPZHE5P1ANZR_7xXYVxciaAVpmW_ktFxw/viewform?c=0&w=1"
 $NewShortcut.Save()
-$list = Get-WinUserLanguageList
-$list.Add("ru-RU")
-Set-WinUserLanguageList $list -Force
+
