5a2056d87b53fc2fb62f68ff376f6150ab373084
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 16:51:07 +0300

    Update and rename start8.ps1 to start9.ps1

--- start9.ps1
+++ start9.ps1
@@ -2,4 +2,4 @@ $username = 'student'
 $password = '1234QWer1234'
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
-Start-Process powershell -ArgumentList ("-file c:/test.ps1")
+Start-Process powershell -Credential $Credential -Verb RunAs -ArgumentList ("-file c:/test.ps1")
