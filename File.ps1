ca0997534678d0e41dcbb577e2b4e42edbf50959
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 14:25:03 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -1,2 +1,5 @@
-"First string " | out-file c:\test.txt #создание файла с одной строкой
-"Second string" | out-file c:\test.txt -append #дописывание информации в конец существующего файла
+"$list = Get-WinUserLanguageList" | out-file c:\test.ps1 #создание файла с одной строкой
+"$list.Add("ru-RU")" | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
+"Set-WinUserLanguageList $list -Force" | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+C:\testq.ps1
