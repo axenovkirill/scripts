451c96136a6dcd88aacc5f5c704b95726ad92c01
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-19 18:37:33 +0300

    Update and rename File.ps1 to File1.ps1

--- File1.ps1
+++ File1.ps1
@@ -2,4 +2,4 @@
 '$list.Add("ru-RU")' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 'Set-WinUserLanguageList -LanguageList $list -Force' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 Set-ExecutionPolicy RemoteSigned -Force
-RegWrite('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run', "confdesktop", "REG_SZ", "c:\test.ps1")
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\test.ps1"  -PropertyType "String"
