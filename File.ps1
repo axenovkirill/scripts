4e057276f301c9550fdc053cc2ea9bc0b0096232
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-20 14:22:23 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -4,5 +4,5 @@
 Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
 Set-ExecutionPolicy RemoteSigned -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
