2b48ad8c0bf95522bbca03c3a4e762df114ac7b8
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-20 14:36:03 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -3,6 +3,8 @@
 #'Set-WinUserLanguageList -LanguageList $list -Force' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
+Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/putty.reg -Destination c:\
 Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
+
