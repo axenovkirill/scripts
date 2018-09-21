51ff354c1313a1c15c20ca0c272a5d8b17e100c5
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-21 08:58:02 +0300

    Update File2.ps1

--- File2.ps1
+++ File2.ps1
@@ -4,8 +4,7 @@
 Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/putty.reg -Destination c:\
-Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/img0.jpeg -Destination C:\Windows\Web\Wallpaper\Windows
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fl.jpeg -Destination C:\
 Set-ExecutionPolicy RemoteSigned -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
