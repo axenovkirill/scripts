0ab59695946743b7bba64a1eef3305e405cc116d
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-21 11:28:19 +0300

    Add files via upload

---
+++ fon.ps1
@@ -0,0 +1,9 @@
+﻿ function Set-NewDesktop
+ {
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\fl.jpeg"
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaperstyle -value "6"
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+ }
+ Set-NewDesktop
+
+ Remove-Item -Path "C:\Users\student\Desktop\Google Chrome.lnk"
