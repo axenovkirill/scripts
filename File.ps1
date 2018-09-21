ad356d212c150d3c1cd4cb587ca4ece8a4c2f902
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-21 11:25:29 +0300

    Update and rename File2.ps1 to File.ps1

--- File.ps1
+++ File.ps1
@@ -5,6 +5,7 @@ Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/putty.reg -Destination c:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fl.jpeg -Destination C:\
+Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fon.ps1 -Destination C:\
 Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
