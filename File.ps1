1296f8e5e61c87150d24222aba9ab3195185eb89
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-21 11:40:54 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -7,7 +7,4 @@ Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
 
-$Time = New-ScheduledTaskTrigger -AtLogOn 
-$A1 = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-executionpolicy bypass C:\fon.ps1"
-$D = New-ScheduledTask -Action $A1 -Trigger $Time
-Register-ScheduledTask T3 -InputObject $D
+
