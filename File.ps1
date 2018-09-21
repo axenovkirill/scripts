bfca4c75519f995627f48e7962ea657a11a621f5
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-21 11:36:48 +0300

    Update and rename File2.ps1 to File.ps1

--- File.ps1
+++ File.ps1
@@ -1,6 +1,3 @@
-#'$list = Get-WinUserLanguageList' | out-file c:\test.ps1 #создание файла с одной строкой
-#'$list.Add("ru-RU")' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
-#'Set-WinUserLanguageList -LanguageList $list -Force' | out-file c:\test.ps1 -append #дописывание информации в конец существующего файла
 Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/putty.reg -Destination c:\
@@ -9,3 +6,8 @@ Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/script
 Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
+
+$Time = New-ScheduledTaskTrigger -AtLogOn 
+$A1 = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-executionpolicy bypass C:\fon.ps1"
+$D = New-ScheduledTask -Action $A1 -Trigger $Time
+Register-ScheduledTask T3 -InputObject $D
