535f696aa8feff21774466a849365441a72202a3
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-29 09:56:39 +0300

    Update File.ps1

--- File.ps1
+++ File.ps1
@@ -5,7 +5,6 @@ Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/script
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fon.ps1 -Destination C:\
 Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
 New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters' -Name "KeepAliveInterval" -Value 0x000003e8  -PropertyType "DWord"
 New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters' -Name "KeepAliveTime" -Value 0x00030d40  -PropertyType "DWord"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
