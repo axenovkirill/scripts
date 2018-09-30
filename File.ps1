a6f6218e1f0dc847858f17c2aafb88722423005e
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-09-30 20:31:46 +0300

    Update and rename File2.ps1 to File.ps1

--- File.ps1
+++ File.ps1
@@ -1,7 +1,7 @@
 Import-Module BitsTransfer
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/ConfVM.ps1 -Destination c:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/putty.reg -Destination c:\
-Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fl.jpeg -Destination C:\
+Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fl.jpg -Destination C:\
 Start-BitsTransfer -Source https://raw.githubusercontent.com/axenovkirill/scripts/master/fon.ps1 -Destination C:\
 Set-ExecutionPolicy RemoteSigned -Force
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name "confdesktop" -Value "powershell.exe -executionpolicy bypass -File c:\ConfVM.ps1"  -PropertyType "String"
