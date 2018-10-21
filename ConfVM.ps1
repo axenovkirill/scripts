e519cbb53594d199fe2e6a9956df199341eb3d14
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-10-21 19:51:27 +0300

    Update ConfVM.ps1

--- ConfVM.ps1
+++ ConfVM.ps1
@@ -146,7 +146,7 @@ Set-PinnedApplication -Action PintoTaskbar -FilePath 'C:\ProgramData\Microsoft\W
 
 Set-PinnedApplication -Action PintoTaskbar -FilePath 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Cisco eReader\Cisco eReader.lnk'
 
-Set-PinnedApplication -Action PintoTaskbar -FilePath 'C:\Program Files (x86)\RlabClient\RemoteLabsClient.lnk'
+Set-PinnedApplication -Action PintoTaskbar -FilePath 'C:\Program Files (x86)\RlabClient\RemoteLabsClient.exe'
 Set-PinnedApplication -Action PintoTaskbar -FilePath 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Foxit Reader\Foxit Reader.lnk'
 
 $tmZone = "Russian Standard Time"
