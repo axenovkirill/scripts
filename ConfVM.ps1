12edd50de0525da945e60169882ff96a51e5608a
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2018-11-26 08:14:44 +0300

    Update ConfVM.ps1

--- ConfVM.ps1
+++ ConfVM.ps1
@@ -5,13 +5,13 @@ $Install_Path = "C:\Users\student\Desktop"
 $WSShell = New-Object -com WScript.Shell
 $ShortcutPath = Join-Path -Path $Install_Path -ChildPath "List registracii.url"
 $NewShortcut = $WSShell.CreateShortcut($ShortcutPath)
-$NewShortcut.TargetPath = "https://goo.gl/forms/qfKT57oppr51KgP63"
+$NewShortcut.TargetPath = "http://80.246.242.87/formtools/modules/form_builder/published/registration_form.php"
 $NewShortcut.Save()
 $Install_Path = "C:\Users\student\Desktop"
 $WSShell = New-Object -com WScript.Shell
 $ShortcutPath = Join-Path -Path $Install_Path -ChildPath "Anketa slushatelya.url"
 $NewShortcut = $WSShell.CreateShortcut($ShortcutPath)
-$NewShortcut.TargetPath = "https://docs.google.com/forms/d/e/1FAIpQLSd3u6D2kdBrOuFyZYPZHE5P1ANZR_7xXYVxciaAVpmW_ktFxw/viewform?c=0&w=1"
+$NewShortcut.TargetPath = "http://80.246.242.87/formtools/modules/form_builder/published/Feedback.php"
 $NewShortcut.Save()
 $Install_Path = "C:\Users\student\Desktop"
 $WSShell = New-Object -com WScript.Shell
